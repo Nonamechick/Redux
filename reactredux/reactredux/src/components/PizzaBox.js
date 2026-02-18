3ac03bbc6aee1776cac5b42c21a246d7e91e0648
Khasan Otazhonov <otajonovxasan350@gmail.com>
2026-02-18 23:01:08 +0500

    7

--- reactredux/reactredux/src/components/PizzaBox.js
+++ reactredux/reactredux/src/components/PizzaBox.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { orderPizza } from './redux';
 
 const PizzaBox = () => {
+  console.log(props);
+  
+
   return (
     <div className='container'>
         <h2 className='text'>Number of pizza available - 100</h2>
         <button className='btn'>Order pizza</button>
     </div>
   )
+}
+
+const mapStateToProps =(state)=>{
+  return{
+    pizzaBase:state.pizzaBase
+  }
+}
+const mapDispatchToProps =(dispatch)=>{
+  return{
+    orderPizza :()=> dispatch(orderPizza())
+  }
 }
 
 export default PizzaBox
